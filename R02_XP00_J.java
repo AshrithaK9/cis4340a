7a9d5ea17e6c9ba5d108d34de48e5455253cdc84
AshrithaK9 <165332066+AshrithaK9@users.noreply.github.com>
2025-03-02 15:08:46 -0500

    Create R02_XP00_J.java

---
+++ R02_XP00_J.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class R02_XP00_J {
+    public void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        if (!someFile.delete()) {
+            System.err.println("Failed to delete the file");
+        }
+    }
+}
